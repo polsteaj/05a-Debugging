a2144aac933a050a39b5603aa95ec98661c28109
polsteaj <polsteaj@rose-hulman.edu>
2018-09-17 00:11:53 -0400

    hi

--- src/m3.py
+++ src/m3.py
@@ -99,7 +99,7 @@ def run_test_broken_1():
 
 
 # ----------------------------------------------------------------------
-# TODO: 3. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 3. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # ----------------------------------------------------------------------
 def broken_1(m):
@@ -121,13 +121,13 @@ def broken_1(m):
     #    **  For full credit you must appropriately
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     count = 0
-    for k in range(2 * m):
-        if is_prime(m):
+    for k in range(m + 2):
+        if is_prime(m + k):
             count = count + 1
-        return count
+    return count
 
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
-    main()
+main()
